a455ccd8e900904ff5d956abc2726d3be49c7ece
Hendrick <hd1867@rit.edu>
2021-09-16 01:27:44 -0400

    Create Recipebook and Dishes

---
+++ Tavern-Taps_Unity/Assets/Scripts/Recipes/Recipe.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct IngredientAmounts
+{
+    public IngredientManager.IngredientTypes Type;
+    [Range(1, 999)]
+    public int Amount;
+}
+
+[CreateAssetMenu]
+public class Recipe : ScriptableObject
+{
+    public List<IngredientAmounts> RequiredIngredients;
+    public Dish FinishedProduct;
+
+    public bool CanCook()
+    {
+        foreach (IngredientAmounts Ingredient in RequiredIngredients)
+        {
+            if (!IngredientManager.Ingredients.checkIngredient(Ingredient.Amount, Ingredient.Type))
+                return false;
+        }
+        return true;
+    }
+
+    public void Cook()
+    {
+        if(CanCook())
+        {
+            foreach (IngredientAmounts Ingredient in RequiredIngredients)
+            {
+                IngredientManager.Ingredients.removeIngredient(Ingredient.Amount, Ingredient.Type);
+            }
+
+            IngredientManager.Ingredients.Dishes.Add(FinishedProduct);
+        }
+    }
+}
