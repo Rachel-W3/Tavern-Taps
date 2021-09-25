8bf9de8a6fb0dae5fa723b72ea28b72c79324755
Hendrick <hd1867@rit.edu>
2021-09-24 23:08:33 -0400

    Complete cooking Implementation

--- Tavern-Taps_Unity/Assets/Scripts/UI/RecipeMenu.cs
+++ Tavern-Taps_Unity/Assets/Scripts/UI/RecipeMenu.cs
@@ -8,7 +8,7 @@ using UnityEditor;
 public class RecipeMenu : MonoBehaviour
 {
     private List<Recipe> recipes = new List<Recipe>();
-    VisualElement recipeTest;
+    private int DishIndex = 0;
 
     public void OnEnable()
     {
@@ -18,6 +18,10 @@ public class RecipeMenu : MonoBehaviour
         //Establish the UI elements you're going to be appending to
         var root = GetComponent<UIDocument>().rootVisualElement;
         var recipeContainer = root.Q<VisualElement>("recipesContainer");
+        
+        //Hide the dish menu[p
+        var DishMenu = root.Q<VisualElement>("DishMenu");
+        DishMenu.style.display = StyleKeyword.None;
 
         //Load the recipe template
         var recipeTemplate = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UI/RecipeMenuItem.uxml");
@@ -32,10 +36,25 @@ public class RecipeMenu : MonoBehaviour
             var recipeName = recipeContainer.Query<Label>().Last();   
             recipeName.text = recipe.FinishedProduct.Name;
 
-            var recipeImage = recipeContainer.Query<VisualElement>().Last();
-            recipeImage.style.backgroundImage = recipe.FinishedProduct.Image;
+            var recipeButton = recipeContainer.Query<Button>().Last();          
+            recipeButton.style.backgroundImage = recipe.FinishedProduct.Image;
+   
+            recipeButton.clicked += () => showDishMenu(recipe);
 
         }
+
+        //Set onclick handlers
+        var DishExitButton = root.Q<Button>("ExitButton");
+        DishExitButton.clicked += hideDishMenu;
+
+        var NextRecipeButton = root.Q<Button>("NextRecipe");
+        NextRecipeButton.clicked += nextRecipe;
+
+        var PrevRecipeButton = root.Q<Button>("PrevRecipe");
+        PrevRecipeButton.clicked += prevRecipe;
+
+        var DishCookButton = root.Q<Button>("CookButton");
+        DishCookButton.clicked += () => recipes[DishIndex].Cook();
     }
 
     private void loadRecipes()
@@ -50,4 +69,69 @@ public class RecipeMenu : MonoBehaviour
             recipes.Add(AssetDatabase.LoadAssetAtPath<Recipe>(AssetDatabase.GUIDToAssetPath(rawRecipe)));
         }
     }
+
+    private void nextRecipe()
+    {
+        DishIndex += 1;
+        if (DishIndex >= recipes.Count)
+            DishIndex = 0;
+
+        showDishMenu(recipes[DishIndex]);
+
+        Debug.Log("Next");
+    }
+
+    private void prevRecipe()
+    {
+        DishIndex -= 1;
+        if (DishIndex < 0)
+            DishIndex = recipes.Count - 1;
+
+        showDishMenu(recipes[DishIndex]);
+
+        Debug.Log("Prev");
+    }
+
+    private void showDishMenu(Recipe recipe)
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        var DishMenu = root.Q<VisualElement>("DishMenu");
+
+        DishIndex = FindRecipeIndex(recipe);
+        Debug.Log(DishIndex);
+
+        if (DishMenu.style.display == StyleKeyword.None)
+        {           
+            DishMenu.style.display = StyleKeyword.Initial;
+        }
+        
+        var DishName = root.Q<Label>("DishName");
+        DishName.text = recipe.FinishedProduct.Name;
+
+        var DishImage = root.Q<VisualElement>("DishImage");
+
+        DishImage.style.backgroundImage = recipe.FinishedProduct.Image;
+
+        var DishDescripton = root.Q<Label>("Description");
+        DishDescripton.text = recipe.FinishedProduct.Description;
+    }
+
+    private void hideDishMenu()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        var DishMenu = root.Q<VisualElement>("DishMenu");
+        DishMenu.style.display = StyleKeyword.None;
+    }
+
+    private int FindRecipeIndex(Recipe recipe)
+    {
+        int index = 0; 
+        for (;index < recipes.Count; index++)
+        {
+            if (recipes[index].FinishedProduct.Name == recipe.FinishedProduct.Name)
+                return index;
+        }
+        return -1;
+    }
+
 }
