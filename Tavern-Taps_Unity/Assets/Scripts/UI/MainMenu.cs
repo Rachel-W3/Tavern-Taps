b79b8e1a6ac12d931c2a96db777fc85b88eaf352
sheroine <42820740+sheroine@users.noreply.github.com>
2021-11-16 13:51:08 -0500

    added dialogue UI file and made textbox

--- Tavern-Taps_Unity/Assets/Scripts/UI/MainMenu.cs
+++ Tavern-Taps_Unity/Assets/Scripts/UI/MainMenu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UIElements; 
+using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
